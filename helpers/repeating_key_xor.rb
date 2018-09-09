09ec001397a7e7035b58c02f5a72f9761ec915be
Jack Slinger <jackslinger10@gmail.com>
2018-09-09 12:35:36 +0100

    Tweeks to the reapearing key xor

--- helpers/repeating_key_xor.rb
+++ helpers/repeating_key_xor.rb
@@ -10,30 +10,15 @@ module Helpers
         end
 
         def encrypt(plain_text)
-            padded_key = repeating_key_of(length: plain_text.size)
-
-            output = plain_text.chars.each_with_index.map do |char, i|
-                moo = (AsciiCoder.encode(char).hex ^ AsciiCoder.encode(padded_key[i]).hex).to_s(16)
-                moo.size == 2 ? moo : "0#{moo}" 
+            plain_text.bytes.each_with_index.map do |byte, i|
+                key_byte = key[i % key.size].ord
+                (byte ^ key_byte).to_s(16).rjust(2, '0')
             end.join('')
-            output
         end
 
         def decrypt(cypher_text)
             # Symetric key and algorithm
             encrypt(cypher_text)
-        end
-
-        private
-
-        def repeating_key_of(length:)
-            division = length / key.size
-            modulo = length % key.size
-            if modulo == 0
-                (key * division)
-            else
-                (key * division) + key[0..modulo-1]
-            end
         end
 
     end
